df69fb9910b4ed65c49108ab66162f0305e73ea5
Daniel Pyon <pyondohyun@gmail.com>
2022-11-22 15:13:55 -0800

    solve phase_two

--- angr/bomb_lab/skeleton.py
+++ angr/bomb_lab/skeleton.py
@@ -1,10 +1,50 @@
 import angr
 
+KABOOM_OFFSET = 0x133b
+
 def phase_one(p, base):
-	pass
+	state = p.factory.blank_state(addr=base+0xf73)
+	sm = p.factory.simulation_manager(state)
+
+	sm.explore(find=base+0xf5d, avoid=base+KABOOM_OFFSET)
+	found = sm.found[0]
+
+	# rdi still holds string
+	mem = found.memory.load(found.regs.rdi, 256)
+	answer = found.solver.eval(mem, cast_to=bytes)
+	out = answer[:answer.index(b'\x00')]
+
+	print(out)
 
 def phase_two(p, base):
-	pass
+	state = p.factory.blank_state(addr=base+0xb22)
+
+	# set up arguments to phase_two
+	arg_one = state.solver.BVS('arg_one', 8*128)
+	arg_one_addr = 0x41414141
+	state.memory.store(arg_one_addr, arg_one)
+	state.add_constraints(state.regs.rdi == arg_one_addr)
+
+	arg_two = state.solver.BVS('arg_two', 8*128)
+	arg_two_addr = 0x42424242
+	state.memory.store(arg_two_addr, arg_two)
+	state.add_constraints(state.regs.rsi == arg_two_addr)
+
+	arg_three = state.solver.BVS('arg_three', 8*128)
+	arg_three_addr = 0x43434343
+	state.memory.store(arg_three_addr, arg_three)
+	state.add_constraints(state.regs.rdx == arg_three_addr)
+
+	sm = p.factory.simulation_manager(state)
+	sm.explore(find=base+0xf5d, avoid=base+KABOOM_OFFSET)
+	found = sm.found[0]
+
+	ret = []
+	for arg in [arg_one, arg_two, arg_three]:
+		answer = found.solver.eval(arg, cast_to=bytes)
+		ret.append(answer[:answer.index(b'\x00')])
+	
+	print(ret)
 
 def phase_three(p, base):
 	pass
